4d894ad0468496e7ec2de4df707cef5179925d71
Aldwin <13520167@std.stei.itb.ac.id>
2023-03-12 16:16:06 +0700

    feat: add frontend and blank files

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const showHelp = (event) => {
+    document.getElementById("modal").style.display = "block";
+}
+
+const closeHelp = (event) => {
+    document.getElementById("modal").style.display = "none";
+}
