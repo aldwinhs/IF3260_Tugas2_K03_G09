9eb25dfe0ac902b0aae69ab9b8646adc1f33836b
Rizky Ramadhana <rizkyramadhana26@gmail.com>
2023-03-25 07:25:21 +0700

    fix light position

--- src/shader/shader.js
+++ src/shader/shader.js
@@ -20,9 +20,9 @@ void main()
 
   highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
   highp vec3 directionalLightColor = vec3(1, 1, 1);
-  highp vec3 directionalVector = normalize(vec3(-3, -3, -3));
+  highp vec3 directionalVector = normalize(vec3(0, 0, 3));
 
-  highp vec4 transformedNormal = uNormalMatrix * vec4(vertNormal, 1.0);
+  highp vec4 transformedNormal = mWorld * vec4(vertNormal, 1.0);
 
   highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
   vLighting = ambientLight + (directionalLightColor * directional);
